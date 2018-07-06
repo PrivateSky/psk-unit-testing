2036b72804dcab8679358d51cea8553c8b04cf36
StefanCosminR <stefancosmin.romanescu@gmail.com>
2018-07-06 17:02:35 +0300

    Repaired a test

--- double-check/assertCallbackAcceptedTypeTest.js
+++ double-check/assertCallbackAcceptedTypeTest.js
@@ -1,5 +1,6 @@
 require("../../../engine/core").enableTesting();
 const assert = $$.requireModule("double-check").assert;
+
 var f = $$.flow.create("acceptedCallbackType",{
     action:function(cb){
        this.cb = cb;
@@ -7,7 +8,19 @@ var f = $$.flow.create("acceptedCallbackType",{
     }
 });
 
-var callbackType = [null, undefined, {}, "string", true, function(cb){f.action(cb);}];
+var wrongCalls = [null, undefined, {}, "string", true, 5, 0, function(){}.toString(), false];
+var goodCalls = [function(cb){f.action(cb);}, (function(callback){return function(cb){f.action(cb);}})()];
+var callbackType = wrongCalls.concat(goodCalls);
+var counter = 0;
+
 for (var i = 0; i < callbackType.length; i++) {
-    assert.callback("acceptedCallbackType", callbackType[i], 1500);
+    try{
+        assert.callback("acceptedCallbackType", callbackType[i], 1500);
+    }catch(error){
+        if(wrongCalls.indexOf(callbackType[i]) !== -1){
+            counter++;
+        }
+    }
 };
+
+assert.equal(counter, wrongCalls.length, "Counter should be equal to "+wrongCalls.length+" instead is "+counter);
