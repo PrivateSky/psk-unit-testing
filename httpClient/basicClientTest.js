354a30a68071b61bf954e9f78b50bc70b4530acb
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2018-07-13 15:08:06 +0300

    basic psk http node client test

---
+++ httpClient/basicClientTest.js
@@ -0,0 +1,37 @@
+require("../../../engine/core");
+const path = require("path");
+$$.requireModule("psk-http-client");
+
+const assert = $$.requireModule("double-check").assert;
+
+const testPort = 9080;
+const alias = "localVirtualMQ";
+const agentName = "007";
+const remote = "http://127.0.0.1:"+testPort;
+const baseFolder = "../../../tmp";
+const mainFolder = "uploads";
+
+assert.callback("BasicNodeHttpClientTest", function(callback){
+	function prepareTest(){
+		var server = $$.requireModule("virtualmq").createVirtualMQ(testPort, path.join(baseFolder, mainFolder), runningTests);
+	}
+
+	function runningTests(){
+		$$.remote.newEndPoint(alias, remote, agentName);
+
+		$$.remote[alias].downloadCSB("superMegaIdCeNuExista", (err, result) => {
+			assert.notNull(err, "Shouldn't work. The file doesn't exist!");
+			assert.equal(err.code, 404, "Status code should be 404");
+
+			clean();
+		});
+	}
+
+	function clean(){
+		callback();
+	}
+
+	prepareTest();
+}, 1500);
+
+
