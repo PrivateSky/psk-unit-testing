0e7e8a47a5a351579bfa1a070b9cc67aa785e887
StefanCosminR <stefancosmin.romanescu@gmail.com>
2018-09-28 15:21:13 +0300

    Added failing test for callflow (serial)

---
+++ callflow/serialTest.js
@@ -0,0 +1,53 @@
+require('../../../builds/devel/pskruntime');
+require("../../../engine/core");
+const assert = require("double-check").assert;
+
+
+const flow = $$.flow.create('serialTest', {
+    init: function (callback) {
+        this.times = [];
+        this.incrementor = incrementor();
+
+        const serial = this.serial(() => {
+            this.times.push(this.incrementor.next().value);
+        });
+
+        serial.__asyncTask(1000, serial.__progress);
+        serial.__asyncTask(1000, this.__progress);
+        serial.__asyncTask(1, serial.__progress);
+
+        setTimeout(() => {
+            assert.equal(this.times.length, 4, 'not all functions executed yet');
+            this.__verifyResults();
+            callback();
+        }, 4000);
+    },
+    __progress: function (err, res) {
+        this.times.push(res.value);
+    },
+    __asyncTask: function (timeout, callback) {
+        let index = this.incrementor.next();
+        setTimeout(() => callback(undefined, index), timeout);
+    },
+    __verifyResults: function () {
+        console.log('times ', this.times);
+        for(let i = 0; i < this.times.length - 1; ++i) {
+            assert.true(this.times[i] <= this.times[i + 1], 'serial callback executed before __progress');
+        }
+    }
+});
+
+
+assert.callback('serialTest', function(callback) {
+    flow.init(callback);
+}, 5000);
+
+
+function* incrementor() {
+    let i = 0;
+    while(i < Infinity) {
+        yield i++;
+    }
+}
+
+
