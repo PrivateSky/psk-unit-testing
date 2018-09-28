3fa5dbb1956af922290245ad92419cc153f759af
asac <asac.cool@gmail.com>
2018-09-28 16:59:02 +0300

    lib tests update

--- libs/libTest.js
+++ libs/libTest.js
@@ -4,29 +4,36 @@ require("../../../builds/devel/pskruntime");
 
 var assert = require("double-check").assert;
 
-var lib1 = $$.loadLibrary("library1","./lib1");
+assert.callback("libTest", function(callback){
 
-var lib2 = $$.loadLibrary("library2","./lib2");
-var lib3 = $$.loadLibrary("library2");  //should be == lib2
+    var lib1 = $$.loadLibrary("library1","./lib1");
 
-assert.notEqual(lib1,null,"lib1 is null");
-assert.notEqual(lib2,null,"lib2 is null");
-assert.equal(lib3,lib2,"Libraries are not the same");
+    var lib2 = $$.loadLibrary("library2","./lib2");
+    var lib3 = $$.loadLibrary("library2");  //should be == lib2
 
-var test1=$$.callflow.start("library1.f1");
-assert.notEqual(test1,null,"Test1 is null");
+    assert.notEqual(lib1,null,"lib1 is null");
+    assert.notEqual(lib2,null,"lib2 is null");
+    assert.equal(lib3,lib2,"Libraries are not the same");
 
-var test2=$$.callflow.start(lib1.f1);
-assert.notEqual(test2,null,"Test2 is null");
+    var test1=$$.callflow.start("library1.f1");
+    assert.notEqual(test1,null,"Test1 is null");
 
-var test3=$$.callflow.start(lib2.f2);
-var test4=$$.callflow.start(lib3.f1);
+    var test2=$$.callflow.start(lib1.f1);
+    assert.notEqual(test2,null,"Test2 is null");
 
-assert.notEqual(test3,null,"Test3 is null");
-assert.notEqual(test4,null,"Test4 is null");
+    var test3=$$.callflow.start(lib2.f2);
+    var test4=$$.callflow.start(lib3.f1);
 
-var lib4 = $$.loadLibrary("library4","./lib2");
-assert.notEqual(lib2, lib4, "Libraries shoud not be the same");
+    assert.notEqual(test3,null,"Test3 is null");
+    assert.notEqual(test4,null,"Test4 is null");
+
+    var lib4 = $$.loadLibrary("library4","./lib2");
+    assert.notEqual(lib2, lib4, "Libraries shoud not be the same");
 
 
-$$.callflow.start("wrongName"); //should send errors
+    $$.callflow.start("wrongName"); //should send errors
+
+    callback();
+
+    setTimeout(()=>{}, 100);
+});
--- libs/testRenameLibrary.js
+++ libs/testRenameLibrary.js
@@ -5,22 +5,23 @@ require("callflow");
 
 var assert = require("double-check").assert;
 
-//var lib1 = $$.loadLibrary("library1", "./lib1");
+assert.callback("RenameLibrary", function(callback){
+    //var lib1 = $$.loadLibrary("library1", "./lib1");
+
+    var lib1 = $$.loadLibrary("library1", require("./lib1"));
+
+    var lib2 = $$.loadLibrary("library2", "./lib1");
+    assert.notEqual(lib1, lib2, "Libraries should not be the same");
+
+    var test1 = $$.callflow.start(lib1.f1);
+    assert.notEqual(test1, null, "Test1 is null");
+
+    var test2 = $$.callflow.start(lib2.f1);
+    assert.notEqual(test2, null, "Test2 is null");
 
 
-var lib1 = $$.loadLibrary("library1", require("./lib1"));
+    var test3 = $$.callflow.start("library2.f1");
+    assert.notEqual(test2, null, "Test3 is null");
 
-var lib2 = $$.loadLibrary("library2", "./lib1");
-assert.notEqual(lib1, lib2, "Libraries should not be the same");
-
-var test1 = $$.callflow.start(lib1.f1);
-assert.notEqual(test1, null, "Test1 is null");
-
-var test2 = $$.callflow.start(lib2.f1);
-assert.notEqual(test2, null, "Test2 is null");
-
-
-var test3 = $$.callflow.start("library2.f1");
-assert.notEqual(test2, null, "Test3 is null");
-
-
+    callback();
+});
