8ae935332ace9ec726855112694ce210c8fd8bec
asac <asac.cool@gmail.com>
2018-10-25 16:57:01 +0300

    bug fixing

--- deployer/configObjectAcceptedTypeTest.js
+++ deployer/configObjectAcceptedTypeTest.js
@@ -6,28 +6,26 @@ var deployer = require(__dirname + "/../../../deployer/Deployer.js");
 var configObject = [[], {}, true, undefined, null, function(){}];
 var errors = [];
 
-for(var i = 0; i < configObject.length; i++ ) {
-    deployer.run(configObject[i], callback);
-}
-
-function callback(error, result) {
-    var testFailed = false;
-    var message = "";
-    if(error) {
-        errors.push(error);
-        message = "[FAIL] " + JSON.stringify(error);
-    } else {
-        console.log(JSON.stringify(result));
+assert.callback("configObjectAcceptedTypeTest", function(cb) {
+    for(var i = 0; i < configObject.length; i++ ) {
+        deployer.run(configObject[i], callback);
     }
-    let logger = testFailed ? console.error : console.log;
-    logger(message);
-};
 
-
-setTimeout(function(){
-    assert.pass("configObjectAcceptedTypeTest", function() {
-        if(errors.length != configObject.length) {
-            throw "Not all negative scenarios of config object has passed!"
+    function callback(error, result) {
+        var testFailed = false;
+        var message = "";
+        if(error) {
+            errors.push(error);
+            message = "[Caught-Expected-Error] " + JSON.stringify(error);
+        } else {
+            console.log(JSON.stringify(result));
         }
-    })
-}, 2000);
+        let logger = testFailed ? console.error : console.log;
+        logger(message);
+    };
+
+    setTimeout(function(){
+        assert.true(errors.length == configObject.length, "Not all negative scenarios of config object has passed!");
+        cb();
+    }, 2000);
+}, 2500);
