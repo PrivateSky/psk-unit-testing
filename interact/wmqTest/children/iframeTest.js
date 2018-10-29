68294b28048c2c271718e55009c0a2c07ae0fdb4
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2018-10-29 15:37:25 +0200

    added karma Window Message Queue interaction test

---
+++ interact/wmqTest/children/iframeTest.js
@@ -0,0 +1,24 @@
+require("callflow");
+require("launcher");
+
+$$.swarm.describe("swarmTest", {
+    public: {
+        collectedValue: "int"
+    },
+    start: function (firstValue, secondValue) {
+        this.value = firstValue;
+        this.swarm("interaction", "step1", this.value, secondValue);
+    },
+    step1: "interaction",
+    step2: function (firstValue, secondValue) {
+        //change order
+        this.swarm("interaction", "step3", secondValue, firstValue);
+    },
+    step3: "interaction",
+    end: function (firstValue, secondValue) {
+        this.swarm("interaction", "step4", firstValue, secondValue);
+    }
+});
+
+var interaction = require("interact").createWindowInteractionSpace("iframe", window.parent);
+interaction.init();
