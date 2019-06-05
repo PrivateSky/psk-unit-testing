9e540888cc362dd9822b1014697069b1cc5fe37c
asac <asac.cool@gmail.com>
2019-06-05 11:42:03 +0300

    fixed example test

--- virtualMQ/existingChannelTest.js
+++ virtualMQ/existingChannelTest.js
@@ -34,118 +34,77 @@ function createServer(callback){
     });
 }
 
-createServer((server)=>{
-    assert.callback("VirtualMQ channel request test", finish=>{
+function createSwarmMessage(){
+    return JSON.stringify({
+        meta:{
+            swarmId: "notArandomId"
+        }
+    });
+}
 
-            const options = {
-                host: '127.0.0.1',
-                port: port,
-                path: '/'+CHANNEL_NAME,
-                method: 'GET'
-            };
+// Make a post with a message
+function postMessage(message){
 
-            http.get(`http://localhost:${port}/${CHANNEL_NAME}`, (resp) => {
-                let data = '';
+    const options = {
+        host: '127.0.0.1',
+        port: port,
+        path: '/'+CHANNEL_NAME,
+        method: 'POST'
+    };
 
-                // A chunk of data has been recieved.
-                resp.on('data', (chunk) => {
-                    data += chunk;
-                });
+    var req = http.request(options, (res) => {
+        const statusCode = res;
+        let error;
+        if (statusCode >= 400) {
+            error = new Error('Request Failed.\n' +
+                `Status Code: ${statusCode}`);
+        }
 
-                // The whole response has been received. Print out the result.
-                resp.on('end', () => {
-                    console.log(JSON.parse(data).explanation);
-                });
+        if (error) {
+            console.log(error);
+            res.resume();
+            return;
+        }
 
-                }).on("error", (err) => {
-                console.log("Error: " + err.message);
-                });
-            
-            // Make a request
-            var req = http.request(options, (res) => {
-                console.log(res);
-                    const statusCode = res;
-                    console.log(statusCode);
-                    let error;
-                    if (statusCode >= 400) {
-                        error = new Error('Request Failed.\n' +
-                            `Status Code: ${statusCode}`);
-                    }
-            
-                    if (error) {
-                        console.log(error);
-                        res.resume();
-                        return;
-                    }
-            
-                    let rawData = '';
-                    console.log("Setting listners");
-                    res.on('data', (chunk) => { rawData += chunk; });
-                    res.on('end', () => {
-                        try {
-                        console.log(rawData);
-                        } catch (err) {
-                            //callback(err);
-                        }
-                    });
-                });
-                req.write("");
-          req.end();
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            console.log(rawData);
+        });
+    });
+    req.write(message);
+    req.end();
+}
 
-       /*client.post('/:testChannel_01', function (req, res) {
-			$$.flow.start("RemoteSwarming").startSwarm(req.params.testChannel_01, req, function (err, result) {
-				res.statusCode = 201;
-				if (err) {
-					console.log(err);
-					res.statusCode = 500;
-				}
-                res.end();
-               //assert.true(201==res.statusCode,`DIDN'T WORK`);
-			});
+function test(finish){
+
+    console.log(`http://localhost:${port}/${CHANNEL_NAME}`);
+
+    //making a get request that will wait until timeout or somebody puts a message on the channel
+    http.get(`http://localhost:${port}/${CHANNEL_NAME}`, (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
         });
 
-        server.get('/:testChannel_01', function(req, res){
-            $$.flow.start("RemoteSwarming").waitForSwarm(req.params.testChannel_01, res, function (err, result, confirmationId) {
-				if (err) {
-					console.log(err);
-					res.statusCode = 500;
-				}else res.statusCode = 201;
-				let responseMessage = result;
-				if((req.query.waitConfirmation || 'false')  === 'false') {
-					res.on('finish', () => {
-						$$.flow.start('RemoteSwarming').confirmSwarm(req.params.testChannel_01, confirmationId, (err) => {});
-					});
-				} else {
-					responseMessage = {result, confirmationId};
-				}
-				res.write(JSON.stringify(responseMessage));
-                res.end();
-                console.log(responseMessage);
-                //assert.true(201==res.statusCode,`DIDN'T WORK`);
-			});
-        })*/
-        //assert.true(201, res.statusCode);
-       
-       
-        // finish();
-       // process.exit(0);
-    })
-}, 50000);
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            assert.equal(createSwarmMessage(), data, "Did not receive the right message back");
+            console.log("Received message", data);
 
+            finish();
+            process.exit(0);
+        });
 
-/*
-var f = $$.flow.describe("Existing Channel",{
-    init:function(cb){
-        this.cb = cb;
-        VirtualMQ.subscribe(CHANNEL_NAME, function(){});
-        this.test();
-    },
-    test:function() {
-        assert.true(VirtualMQ.hasChannel(CHANNEL_NAME) == true, "Could not subscribe")
-        this.cb();
-    }
-})();
-assert.callback("Existing Channel", function(callback){
-    f.init(callback);
-},1500);
-*/
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+
+    postMessage(createSwarmMessage());
+}
+
+createServer((server)=> {
+    assert.callback("VirtualMQ channel request test",test, 10000);
+});
