16e8c2b86167bf62779b7b01269fcdc839c80763
adrianganga@yahoo.com <adiganga2002@yahoo.com>
2019-06-19 18:59:42 +0300

    Add simultaneous read test from virtualMQ 2 scenarios and create an utils lib for virtualMQ

---
+++ Utils/virtualMQUtils.js
@@ -0,0 +1,124 @@
+require("../../../builds/devel/pskruntime");
+require("../../../builds/devel/virtualMQ");
+require("../../../builds/devel/psknode");
+const http = require("http");
+
+const VirtualMQ = require('virtualmq');
+const CHANNEL_NAME = Buffer.from('mychannel').toString('base64');
+const path = require("path");
+const fs = require('fs');
+let port = 8089;
+let serverURL = `http://localhost:${port}/${CHANNEL_NAME}`;
+
+
+deleteFolder = function (folder) {
+    if (fs.existsSync(folder)) {
+        fs.readdirSync(folder).forEach(function (file, index) {
+            var curPath = path.join(folder, file);
+            if (fs.lstatSync(curPath).isDirectory()) {
+                deleteFolder(curPath);
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(folder);
+    }
+};
+module.exports.serverUrl = serverURL;
+module.exports.initVirtualMQ = function () {
+    this.createTestFolder();
+};
+
+module.exports.createTestFolder = function () {
+
+    try {
+        folder = fs.mkdtempSync("getMsg");
+    } catch (err) {
+        console.log("Failed to create tmp directory");
+        return;
+    }
+
+};
+module.exports.createServer = function (callback) {
+    var server = VirtualMQ.createVirtualMQ(port, folder, undefined, (err, res) => {
+        if (err) {
+            console.log("Failed to create VirtualMQ server on port ", port);
+            console.log("Trying again...");
+            if (port > 1024 && port < 50000) {
+                port++;
+                createServer(callback);
+            } else {
+                console.log("There is no available port to start VirtualMQ instance need it for test!");
+            }
+        } else {
+            console.log("Server ready and available on port ", port);
+            serverURL = `http://localhost:${port}/${CHANNEL_NAME}`;
+            callback(server);
+        }
+    });
+};
+
+module.exports.getRequestOptions = function (requestType) {
+    return {
+        host: '127.0.0.1',
+        port: port,
+        path: '/' + CHANNEL_NAME,
+        method: requestType || 'POST'
+    }
+};
+
+module.exports.createSwarmMessage = function (msg) {
+    return JSON.stringify({
+        meta: {
+            swarmId: msg
+        }
+    });
+}
+
+module.exports.deleteFolder = deleteFolder;
+
+
+/*
+HTTP Request with args:
+ - msg: string message to POST/PUT
+ - responseCallback: callback for on end request, default will just display a message
+ - requestType : PUT, POST, GET, DELETE (default is POST)
+* */
+module.exports.httpRequest = function (msg, responseCallback, requestType) {
+    let opt = this.getRequestOptions(requestType);
+    var req = http.request(opt, (res) => {
+        const statusCode = res.statusCode;
+        let error;
+        if (statusCode >= 400) {
+            error = new Error('Request Failed.\n' +
+                `Status Code: ${statusCode}`);
+        }
+
+        if (error) {
+            console.log(error);
+            res.resume();
+            return;
+        }
+
+        let rawData = '';
+        res.on('data', (chunk) => {
+            rawData += chunk;
+        });
+        let onEndCallback = responseCallback || function (rowData) {
+            console.log('posting data', rowData)
+        };
+        res.on('end', () => {
+            onEndCallback(rawData)
+        });
+    });
+    if (opt.method != 'GET') {
+        req.write(msg);
+    }
+    req.end();
+};
+module.exports.cleanUp = function (timeout) {
+    setTimeout(() => {
+        this.deleteFolder(folder);
+        process.exit(0);
+    }, timeout);
+};
