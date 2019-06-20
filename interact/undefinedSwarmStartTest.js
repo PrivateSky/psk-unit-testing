676b67c25f4124a9afe24c0a823564019850ca36
Vlad <lupuvlad2000@gmail.com>
2019-06-20 11:57:09 +0300

    test changing arguments in interaction/ test starting undefined swarms + defined ones after

---
+++ interact/undefinedSwarmStartTest.js
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////
+// Tests if the arguments passed through the interact module
+// to the target swarm are received as expected
+/////////////////////////////////////////////////////////////
+require('../../../builds/devel/pskruntime');
+require('../../../builds/devel/psknode');
+const assert = require('double-check').assert;
+const interact = require('../../../modules/interact');
+const arg1 = { a: 'asd', b: 'eqd' };
+const arg2 = 2;
+
+$$.swarm.describe('interactSwarm', {
+  doSomething: function() {
+    console.log('Do something interactSwarm');
+    this.return(true);
+  }
+});
+$$.swarm.describe('interactSwarm2', {
+  doSomething: function() {
+    console.log('Do something interactSwarm2');
+    this.return(true);
+  }
+});
+
+assert.callback(
+  'Starting undefined swarms and defined swarms',
+  finished => {
+    const iSpace = interact.createInteractionSpace();
+    iSpace.startSwarm('interactSwarm33', 'doSomething');
+    iSpace.startSwarm('interactSwarm44', 'doSomething');
+    iSpace.startSwarm('interactSwarm55', 'doSomething');
+    iSpace.startSwarm('interactSwarm2', 'doSomething').onReturn(res => {
+      counter += 1;
+    });
+    iSpace.startSwarm('interactSwarm3', 'doSomething');
+    iSpace.startSwarm('interactSwarm4', 'doSomething');
+    iSpace.startSwarm('interactSwarm5', 'doSomething');
+    iSpace.startSwarm('interactSwarm', 'doSomething').onReturn(res => {
+      counter += 1;
+    });
+    let counter = 0;
+    setTimeout(() => {
+      assert.true(counter == 2, `Calling a defined swarm after an undefined one doesn't work`);
+      finished();
+      process.exit(0);
+    }, 2000);
+  },
+  2000
+);
