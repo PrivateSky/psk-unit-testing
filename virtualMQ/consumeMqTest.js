9e681edff08a51037d2823705123eda82e548901
Claudia Florea <claudia.florea@heavensolutions.com>
2019-06-21 16:13:07 +0300

    tried to transpose to utils library. to be reviewed

--- virtualMQ/consumeMqTest.js
+++ virtualMQ/consumeMqTest.js
@@ -1,185 +1,80 @@
 /* This test aims to test if deletion of all the messages in a queue is working properly */
-
-require("../../../builds/devel/pskruntime");
-require("../../../builds/devel/virtualMQ");
-require("../../../builds/devel/psknode");
-
-const utils  = require("../Utils/virtualMQUtils");
+const utils = require("../Utils/virtualMQUtils");
 const assert = require("double-check").assert;
-const VirtualMQ = require('virtualmq');
-const CHANNEL_NAME = Buffer.from('testChannel').toString('base64');
-const fs = require("fs");
-const http = require("http");
-var folder;
+
 var index = 0;
-var nrOfDeletions=0;
+var nrOfDeletions = 0;
 var msgArr = ['msg_1', 'msg_2', 'msg_3', 'msg_4', 'msg_5'];
 var numberOfMessages = msgArr.length;
-var intervalId = null;
+var intervalId = null, finishCallback;
 
-try {
-    folder = fs.mkdtempSync("testFile");
-} catch (err) {
-    console.log("Failed to create tmp directory");
-}
 
-let port = 8092;
-function createServer(callback) {
-    var server = VirtualMQ.createVirtualMQ(port, folder, undefined, (err, res) => {
-        if (err) {
-            console.log("Failed to create VirtualMQ server on port ", port);
-            console.log("Trying again...");
-            if (port > 0 && port < 50000) {
-                port++;
-                createServer(callback);
-            } else {
-                console.log("There is no available port to start VirtualMQ instance need it for test!");
-            }
-        } else {
-            console.log("Server ready and available on port ", port);
-            callback(server);
-        }
-    });
-}
+let postCallback = function () {
+    console.log('postCallback index = ', index);
+    index++;
+    if (index == msgArr.length) {
+        return;
+    }
+    if (index == 1) {
+        // just wait for file structure to be created
+        setTimeout(() => {
+            postCallback(utils.createSwarmMessage(msgArr[index]), postCallback);
+        }, 5000);
+    } else {
+        postCallback(utils.createSwarmMessage(msgArr[index]), postCallback);
+    }
+};
+let getMessageIdCallback = function (data) {
+    console.log('message id =' , JSON.parse(data).confirmationId);
+    console.log('nrOfDeletions initial = ' + nrOfDeletions + ' and msgArr.length initial = ' + msgArr.length );
 
-function createSwarmMessage(msg) {
-    return JSON.stringify({
-        meta: {
-            swarmId: msg
-        }
-    });
-}
+    if (nrOfDeletions + 1 < 5 || msgArr.length > 0) {
+        deleteMessage(JSON.parse(data).confirmationId);
+        msgArr.shift();
+        nrOfDeletions++;
+        console.log('nrOfDeletions ' + nrOfDeletions + ' msgArr.length  = ' + msgArr.length );
+    } else {
+        clearInterval()
+    }
+};
 
-// Make a post with a message
-function postMessage(message, type) {
-    const options = {
-        host: '127.0.0.1',
-        port: port,
-        path: '/' + CHANNEL_NAME,
-        method: type || 'POST'
-    };
-
-    var req = http.request(options, (res) => {
-        const statusCode = res;
-        let error;
-        if (statusCode >= 400) {
-            error = new Error('Request Failed.\n' +
-                `Status Code: ${statusCode}`);
-        }
-
-        if (error) {
-            console.log(error);
-            res.resume();
-            return;
-        }
-
-        let rawData = '';
-        res.on('data', (chunk) => {
-            rawData += chunk;
-        });
-        res.on('end', () => {
-            index++;
-            if(index == msgArr.length){
-                return;
-            }
-            if(index==1){
-            // just wait for file structure to be created
-                setTimeout(()=>{
-                    postMessage(createSwarmMessage(msgArr[index]));
-                }, 5000);
-            }
-            else{
-                postMessage(createSwarmMessage(msgArr[index]));
-            }
-        });
-    });
-    req.write(message);
-    req.end();
-}
+let deleteMessageCallback = function (data) {
+    if (nrOfDeletions == numberOfMessages) {
+        clearInterval(intervalId);
+        finishCallback();
+        assert.equal(0, msgArr.length, "Queue is not empty");
+        utils.deleteFolder(folder);
+        process.exit(0);
+    }
+};
 
 // Make a delete request
-function deleteMessage(msgId, finish) {
-
-    const options = {
-        host: '127.0.0.1',
-        port: port,
-        path: '/' + CHANNEL_NAME + '/'+ msgId,
-        method: 'DELETE',
-        };
-
-    var req = http.request(options, (res) => {
-        const statusCode = res;
-        let error;
-        if (statusCode >= 400) {
-            error = new Error('Request Failed.\n' +
-                `Status Code: ${statusCode}`);
-        }
-
-        if (error) {
-            console.log(error);
-            res.resume();
-            return;
-        }
-
-        let rawData = '';
-        res.on('data', (chunk) => {
-            rawData +=chunk;
-        });
-
-        res.on('end', () => {
-            msgArr.shift();
-            if(nrOfDeletions==numberOfMessages){
-                clearInterval(intervalId);
-                finish();
-                assert.equal(0, msgArr.length, "Queue is not empty");
-                utils.deleteFolder(folder); 
-                process.exit(0);
-            }
-        });
-    });
-    req.write(msgId);
-    req.end();
+function deleteMessage(msgId) {
+    let options = utils.getRequestOptions('DELETE', '/' + msgId);
+    utils.httpRequest(msgId, deleteMessageCallback, 'DELETE', options);
 }
 
 function deleteMessageFromMQ(finish) {
-    //making a get request that will wait find out the confirmation id
-    let url = `http://localhost:${port}/${CHANNEL_NAME}?waitConfirmation=true`;
-    http.get(url, (resp) => {
-        let data = '';
-
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
-       
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {            
-        if (nrOfDeletions+1<5 || msgArr.length>0) {
-            deleteMessage(JSON.parse(data).confirmationId, finish);
-            nrOfDeletions++;
-        }else{
-            clearInterval()
-        }
-        });  
-   
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-    }).end();
+    let options = utils.getRequestOptions('GET', '?waitConfirmation=true');
+    utils.httpRequest(null, getMessageIdCallback, 'GET', options);
 }
 
 function test(finish) {
-    postMessage(createSwarmMessage(msgArr[index]));
+    finishCallback = finish;
+    utils.createServer((server) => {
+        utils.httpRequest(utils.createSwarmMessage(msgArr[index]), postCallback);
 
-    //delete first message from queue
-    intervalId = setTimeout(()=>{
-     setInterval(() => {
-          deleteMessageFromMQ(finish);
-       }, 500); 
-    }, 5000);
-};
-
-createServer((server) => {
-    assert.callback("VirtualMQ dequeue test", test, 15000);
-});
+        //delete every message from queue
+        intervalId = setTimeout(() => {
+            setInterval(() => {
+                deleteMessageFromMQ(finish);
+            }, 500);
+        }, 5000);
+    });
+}
 
 
+utils.initVirtualMQ();
+assert.callback("VirtualMQ GET request test", test, 25000);
+//delete cretead test folder
+utils.cleanUp(25000 + 3000);
--- virtualMQ/messageReadingTest.js
+++ virtualMQ/messageReadingTest.js
@@ -2,210 +2,90 @@
  than the swarm messages in JSON format.
  The test should pass, but should show some errors in the console due to the bad message format */
 
-require("../../../builds/devel/pskruntime");
-require("../../../builds/devel/virtualMQ");
-require("../../../builds/devel/psknode");
-
 const utils  = require("../Utils/virtualMQUtils");
 const assert = require("double-check").assert;
-const VirtualMQ = require('virtualmq');
-const CHANNEL_NAME = Buffer.from('testChannel').toString('base64');
-let port = 8092;
-var index = 0;
-const fs = require("fs");
-const http = require("http");
-var folder;
-var timer1 = null;
-var timer2 = null;
+var index = 0;swarmIndex=0;
 var msgArr = ['msg_1', 'msg_2', 'msg_3', 'msg_4', 'msg_5'];
 var newArr = ['msg_6', 'msg_7', 'msg_8', 'msg_9', 'msg_10'];
 var countMsg = 0;
 
-try {
-    folder = fs.mkdtempSync("folder");
-} catch (err) {
-    console.log("Failed to create tmp folder");
-}
-
-//var server = new Server(sslConfig).listen(port);
-function createServer(callback) {
-    var server = VirtualMQ.createVirtualMQ(port, folder, undefined, (err, res) => {
-        if (err) {
-            console.log("Failed to create VirtualMQ server on port ", port);
-            console.log("Trying again...");
-            if (port > 0 && port < 50000) {
-                port++;
-                createServer(callback);
-            } else {
-                console.log("There is no available port to start VirtualMQ instance need it for test!");
-            }
-        } else {
-            console.log("Server ready and available on port ", port);
-            callback(server);
-        }
-    });
-}
-
-function createSwarmMessage(msg) {
-    return JSON.stringify({
-        meta: {
-            swarmId: msg
-        }
-    });
-}
-
-// Make a bad post request with string format by simply passing the array elements
-function postBadMessage(message, type) {
-
-    const options = {
-        host: '127.0.0.1',
-        port: port,
-        path: '/' + CHANNEL_NAME,
-        method: type || 'POST'
-    };
-
-    var req = http.request(options, (res) => {
-        const statusCode = res;
-        let error;
-        if (statusCode >= 400) {
-            error = new Error('Request Failed.\n' +
-                `Status Code: ${statusCode}`);
-        }
-
-        if (error) {
-            console.log(error);
-            res.resume();
+let stringMessage= function(msg){
+    console.log("Post bad message", msg);
+    console.log('index= ', index);
+    index++;
+    try{
+        if(index == newArr.length){
             return;
         }
-
-        let rawData = '';
-        res.on('data', (chunk) => {
-            rawData += chunk;
-        });
-        res.on('end', () => {
-            console.log("Post bad message", message);
-            index++;
-            if(index == newArr.length-1){
-                clearInterval(timer1);
-            }
-            if(index == newArr.length){
-                return;
-            }
-           if(index==1){
-                setTimeout(()=>{
-                    postBadMessage(newArr[index]);
-                }, 2000);
-            }
-           else{
-                postBadMessage(newArr[index]);
-            }
-        });
-    });
-    req.write(message);
-    req.end();
+        if(index==1){
+            setTimeout(()=>{
+                utils.httpRequest(newArr[index], stringMessage);
+            }, 5000);
+        }
+        else{
+            utils.httpRequest(newArr[index], stringMessage)
+        }
+    }catch(err){
+        console.log('Error ' , err);
+        index++
+    }   
 }
 
-// Make a good post with JSON format, using the createSwarmMessage function
-function postMessage(message, type) {
-    const options = {
-        host: '127.0.0.1',
-        port: port,
-        path: '/' + CHANNEL_NAME,
-        method: type || 'POST'
-    };
+let swarmMessage= function(msg){
+    console.log("Post good message", msg);
+    swarmIndex++;
+    if(swarmIndex == msgArr.length){
+        return;
+    }
+    if(swarmIndex==1){
+        setTimeout(()=>{
+            utils.httpRequest(utils.createSwarmMessage(msgArr[swarmIndex]), swarmMessage);
+        }, 5000);
+    }
+    else{
+        utils.httpRequest(utils.createSwarmMessage(msgArr[swarmIndex]), swarmMessage)
+    }
+}
 
-    var req = http.request(options, (res) => {
-        const statusCode = res;
-        let error;
-        if (statusCode >= 400) {
-            error = new Error('Request Failed.\n' +
-                `Status Code: ${statusCode}`);
-        }
+let verify = function(data){
+    var expected = utils.createSwarmMessage(msgArr[countMsg]);
+    countMsg++;
+    console.log("Got message", data);
+    console.log("Expected message ", expected);
+    assert.equal(expected, data, "Did not receive the right message back");
 
-        if (error) {
-            console.log(error);
-            res.resume();
-            return;
-        }
-
-        let rawData = '';
-        res.on('data', (chunk) => {
-            rawData += chunk;
-        });
-        res.on('end', () => {
-            console.log("Post good message", message);
-            index++;
-            if(index == msgArr.length-1){
-                clearInterval(timer2);
-            }
-            if(index == msgArr.length){
-                return;
-            }
-            if(index==1){
-            // just wait for file structure to be created
-                setTimeout(()=>{
-                    postMessage(createSwarmMessage(msgArr[index]));
-                }, 5000);
-            }
-            else{
-                postMessage(createSwarmMessage(msgArr[index]));
-            }
-        });
-    });
-    req.write(message);
-    req.end();
+    if (countMsg == msgArr.length) {
+        utils.deleteFolder(folder);
+        finish();
+        process.exit(0);
+    }
 }
 
 function getMessageFromQueue(finish) {
     //making a get request that will wait until timeout or somebody puts a message on the channel
-    http.get(`http://localhost:${port}/${CHANNEL_NAME}`, (resp) => {
-        let data = '';
-
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            var expected = createSwarmMessage(msgArr[countMsg]);
-            countMsg++;
-            console.log("Got message", data);
-            console.log("Expected message ", expected);
-            assert.equal(expected, data, "Did not receive the right message back");
-
-            if (countMsg == msgArr.length) {
-                utils.deleteFolder(folder);
-                finish();
-                process.exit(0);
-            }
-        });
-
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-    }).end();
-
+    utils.httpRequest(null, verify, 'GET');
 }
 
-function test(finish) {
-    setTimeout(()=>{
-        setInterval(()=> {getMessageFromQueue(finish)},100);
-    }, 20000);
+function test(finish) { 
+    utils.createServer((server) => {
+        //here we try to post some messages as strings (not valid message)
+        utils.httpRequest(newArr[index], stringMessage);
 
-    //here we try to post some messages as strings
-    timer1= setTimeout(()=>{postBadMessage(newArr[index])
-    }, 5000);
+        //here we sent some messages as JSON 
+        setTimeout(()=>{
+            utils.httpRequest(msgArr[swarmIndex], swarmMessage);
+        }, 20000);
 
-    //here we sent some messages as JSON 
-    timer2= setTimeout(()=>{
-      postMessage(createSwarmMessage(msgArr[index]))
-    }, 10000);
-
-    //here we try to resend the messages as strings
-    setTimeout(()=>{postBadMessage(newArr[index])
-    }, 17000);
+        setTimeout(()=>{
+            setInterval(()=> {
+                getMessageFromQueue(finish)
+            },100);
+        }, 26000); 
+    });
 }
-  
-createServer((server) => {
-    assert.callback("VirtualMQ channel request test", test, 30000);
-});
+//creating the test folder
+utils.initVirtualMQ();
+//calling the test function
+assert.callback("VirtualMQ POSSSSST request test", test, 40000);
+//delete created test folder
+utils.cleanUp(45000);
