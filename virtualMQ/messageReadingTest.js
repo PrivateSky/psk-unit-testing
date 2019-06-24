52b928fc5938d5d9a51403eb376fce9e94404b9c
adrianganga@yahoo.com <adiganga2002@yahoo.com>
2019-06-24 14:29:01 +0300

    Refactor test for invalid MQ msessage (fails as server is not throwing error)

--- virtualMQ/messageReadingTest.js
+++ virtualMQ/messageReadingTest.js
@@ -2,87 +2,104 @@
  than the swarm messages in JSON format.
  The test should pass, but should show some errors in the console due to the bad message format */
 
-const utils  = require("../Utils/virtualMQUtils");
+const utils = require("../Utils/virtualMQUtils");
 const assert = require("double-check").assert;
-var index = 0;swarmIndex=0;
+var index = 0;
+swarmIndex = 0;
 var msgArr = ['msg_1', 'msg_2', 'msg_3', 'msg_4', 'msg_5'];
-var newArr = ['msg_6', 'msg_7', 'msg_8', 'msg_9', 'msg_10'];
-var countMsg = 0;
+var invalidMsgArr = ['msg_6', 'msg_7', 'msg_8', 'msg_9', 'msg_10'];
+var countMsg = 0,countInvalidMsg = 0, getMessageInterval, finishCallback;
 
-let stringMessage= function(msg){
-    console.log("Post bad message", msg);
-    console.log('index= ', index);
+let stringMessage = async function (msg) {
+    console.log('------------ index', index);
     index++;
-    try{
-        if(index == newArr.length){
+    try {
+        if (index == invalidMsgArr.length) {
+            assert.equals(countInvalidMsg,invalidMsgArr.length, 'Wrong number of invalid messages');
             return;
         }
-        if(index==1){
-            setTimeout(()=>{
-                utils.httpRequest(newArr[index], stringMessage);
+        if (index == 1) {
+            setTimeout(() => {
+                utils.httpRequest(invalidMsgArr[index], stringMessage);
             }, 5000);
+        } else {
+            utils.httpRequest(invalidMsgArr[index], stringMessage);
         }
-        else{
-            utils.httpRequest(newArr[index], stringMessage)
-        }
-    }catch(err){
-        console.log('Error ' , err);
-        index++
-    }   
-}
 
-let swarmMessage= function(msg){
+    } catch (err) {
+        console.log('Error ---------- ', err);
+        index++;
+        countInvalidMsg++;
+    }
+};
+
+let swarmMessage = function (msg) {
     console.log("Post good message", msg);
     swarmIndex++;
-    if(swarmIndex == msgArr.length){
+    if (swarmIndex == msgArr.length) {
         return;
     }
-    if(swarmIndex==1){
-        setTimeout(()=>{
+    if (swarmIndex == 1) {
+        setTimeout(() => {
             utils.httpRequest(utils.createSwarmMessage(msgArr[swarmIndex]), swarmMessage);
         }, 5000);
-    }
-    else{
+    } else {
         utils.httpRequest(utils.createSwarmMessage(msgArr[swarmIndex]), swarmMessage)
     }
-}
+};
 
-let verify = function(data){
+let verify = function (data) {
     var expected = utils.createSwarmMessage(msgArr[countMsg]);
     countMsg++;
     console.log("Got message", data);
     console.log("Expected message ", expected);
-    assert.equal(expected, data, "Did not receive the right message back");
+
 
     if (countMsg == msgArr.length) {
+        clearInterval(getMessageInterval);
         utils.deleteFolder(folder);
-        finish();
+        finishCallback();
         process.exit(0);
+    }else{
+        assert.equal(expected, data, "Did not receive the right message back");
     }
-}
+};
 
 function getMessageFromQueue(finish) {
+    finishCallback = finish;
     //making a get request that will wait until timeout or somebody puts a message on the channel
     utils.httpRequest(null, verify, 'GET');
 }
 
-function test(finish) { 
+function test(finish) {
     utils.createServer((server) => {
-        //here we try to post some messages as strings (not valid message)
-        utils.httpRequest(newArr[index], stringMessage);
+        //stringMessage();
+        try {
+            //here we try to post some messages as strings (not valid messages)
+            utils.httpRequest(invalidMsgArr[index], stringMessage);
+        } catch (e) {
+            console.log(' Message could not be posted --------');
+        }
 
-        //here we sent some messages as JSON 
-        setTimeout(()=>{
-            utils.httpRequest(msgArr[swarmIndex], swarmMessage);
+        //here we sent some messages as JSON
+
+        setTimeout(() => {
+            assert.equal(countInvalidMsg,invalidMsgArr.length, 'Wrong number of invalid messages');
+            utils.httpRequest(utils.createSwarmMessage(msgArr[swarmIndex]), swarmMessage);
         }, 20000);
 
-        setTimeout(()=>{
-            setInterval(()=> {
+        setTimeout(() => {
+            getMessageInterval = setInterval(() => {
                 getMessageFromQueue(finish)
-            },100);
-        }, 26000); 
+            }, 100);
+        }, 26000);
     });
 }
+
+process.on('uncaughtException', function (err) {
+        console.log('MQ Fail ', err);
+    }
+);
 //creating the test folder
 utils.initVirtualMQ();
 //calling the test function
